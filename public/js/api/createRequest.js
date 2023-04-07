58337b8525e24a3ec73c1fc8affde4bc0a34c6c0
Artem <artembl487@gmail.com>
2023-04-07 22:43:11 +0300

    mc

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -4,4 +4,29 @@
  * */
 const createRequest = (options = {}) => {
 
+    function createUrlSearchParam(obj){
+        let sp = new URLSearchParams(obj);
+        return sp.toString();
+    }
+
+    let dataParam = createUrlSearchParam(options.data);
+    let url = new URL(options.url);
+    url.search = dataParam;
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    
+    if(options.method === "GET"){
+
+    try{
+    xhr.open(options.method, url);
+    xhr.send();
+    }
+    catch(e){
+         // перехват сетевой ошибки
+      callback( e );
+    }
+}
 };
+
+
